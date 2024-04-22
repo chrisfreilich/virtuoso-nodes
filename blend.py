a4c53e0269f94d70b15eed8ec8bccf7cfcd9ad7c
chrisfreilich <chrisfreilich@gmail.com>
2024-04-22 17:13:28 -0400

    Working in Comfy

--- blend.py
+++ blend.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 from blend_modes import difference
+import torch
 
 class Blend:
     
@@ -33,28 +34,19 @@ class Blend:
     def do_blend(self, backdrop, source, opacity, source_adjust, blend_mode):
 
         # Ensure images are RGBA
-        # backdrop_rgba = Image.fromarray(backdrop).convert('RGBA')
-        # source_rgba = Image.fromarray(source).convert('RGBA')
-
-        backdrop_rgba = backdrop.convert('RGBA')
-        source_rgba = source.convert('RGBA')
+        backdrop_prepped = prep_image(backdrop)
+        source_prepped = prep_image(source)
 
         # Size source image
-        width, height = backdrop_rgba.size
-        source_rgba = source_rgba.resize((width, height))
-        
-        # Convert images back to numpy arrays
-        backdrop = np.array(backdrop_rgba).astype(float)
-        source = np.array(source_rgba).astype(float)
+        # width, height = backdrop_rgba.size
+        # source_rgba = source_rgba.resize((width, height))
 
         # Apply the blend mode
-        blended_array = difference(backdrop, source, opacity=1)
+        blended_np = difference(backdrop_prepped, source_prepped, opacity=1)
 
-        # Convert the result to an 8-bit array and make an image from it
-        blended_image = Image.fromarray(blended_array.astype(np.uint8))
+        final_tensor = (torch.from_numpy(blended_np / 255)).unsqueeze(0)
 
-        #do some processing on the image, in this example I just invert it
-        return (blended_image,)
+        return (final_tensor,)
 
     """
         The node will always be re executed if any of the inputs change but
@@ -68,3 +60,25 @@ class Blend:
     #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
     #    return ""
 
+def prep_image(img):
+    # Check if the image has a batch dimension and if so, remove it
+    if img.shape[0] == 1:
+        img = img.squeeze(0)
+
+    # Convert image from 0-1 to 0-255 data
+    img = img * 255
+
+    # Check if the image has an alpha channel
+    if img.shape[2] == 4:
+        return img
+    elif img.shape[2] == 3:
+        # Create a new tensor with the same height and width as the input image
+        # and a single channel filled with the maximum value (representing full opacity)
+        alpha_channel = torch.full((img.shape[0], img.shape[1], 1), fill_value=255, dtype=img.dtype, device=img.device)
+        
+        # Concatenate the input image with the new alpha channel along the channel dimension
+        img_with_alpha = torch.cat((img, alpha_channel), dim=2)
+        
+        return img_with_alpha.numpy()
+    else:
+        raise ValueError("Input image must have either 3 (RGB) or 4 (RGBA) channels")
